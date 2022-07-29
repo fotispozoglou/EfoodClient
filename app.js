8e842c90151f0a0c762377a2b7fcdcdcac51d2bb
fotis_pozoglou <fotispozoglou@protonmail.com>
2022-07-29 19:36:43 +0300

    Configuring For Heroku Deployment

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ const sessionConfig = {
   name: 'sessionID',
   cookie: {
     httpOnly: true,
-    // secure: true,
+    secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
